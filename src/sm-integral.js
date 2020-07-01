714fe706e52274295fad06799751a2ffe518ad4a
peelstnac <chfrn2002@gmail.com>
2020-06-30 21:24:47 -0400

    Added class layout

---
+++ src/sm-integral.js
@@ -0,0 +1,22 @@
+class Integral {
+    //there are two possible types: romberg and carlo
+    constructor(type) {
+        this.type = type;
+    }
+    //pass in function, lower bound, upper bound, order of error
+    //bounds can be numbers as well as "-inf"/"inf"
+    integrate(f, a, b, e) {
+        function rombergDefinite() {
+
+        }
+        function rombergImproper() {
+
+        }
+        function carloDefinite() {
+            
+        }
+        function carloImproper() {
+            
+        }
+    }
+}
